fe3078d1c4c5d277f263c61a3ac688cb4ccf939a
pglasserman <pg20@columbia.edu>
2026-03-11 13:54:30 -0400

    Update reg_tool.py

--- reg_tool.py
+++ reg_tool.py
@@ -26,7 +26,7 @@ Features:
 import streamlit as st
 import pandas as pd
 import numpy as np
-from scikit-learn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 import statsmodels.api as sm
 
 
